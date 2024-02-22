c9cfd15b158e16ad975c354a1cf7cc7c68d4a5fb
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-02-22 22:34:00 +0700

    adding dark mode

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BellDot } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from '@/components/ui/toggle'
 
@@ -7,7 +6,6 @@ function Header() {
   return (
     <div className="p-4 dark:bg-slate-900 bg-white flex justify-end shadow-md">
         <div className="flex items-center gap-4">
-            <BellDot />
             <Button className="ml-4 dark:text-white">Sign in</Button>
             <ModeToggle />
         </div>
