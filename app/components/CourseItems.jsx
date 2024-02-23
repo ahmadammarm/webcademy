1fb5b5dc7773c409917a68427273bc2c174797fe
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-02-23 09:14:10 +0700

    adding banner and course list

--- app/components/CourseItems.jsx
+++ app/components/CourseItems.jsx
@@ -1,28 +1,53 @@
 import React from "react";
 import Image from "next/image";
 
-function CourseItems() {
+const courseItemCardData = [
+  {
+    image: "/html/banner-html.jpg",
+    title: "Belajar HTML",
+    author: "Sandhika Galih",
+    link: "./html"
+  },
+  {
+    image: "/html/css-banner.jpg",
+    title: "Belajar CSS",
+    author: "Sandhika Galih",
+    link: "./css"
+  },
+  {
+    image: "/html/javascript.jpg",
+    title: "Belajar Javascript",
+    author: "Sandhika Galih",
+    link: "./javascript"
+  }
+];
+
+const CourseItemCard = ({ image, title, author, link }) => {
   return (
-    <>
-      <div className="flex lg:flex-row md:flex-col gap-5">
-        <div className="course-item-card flex flex-col">
-            <Image src="/html/banner-html.jpg" width={500} height={150} className="rounded-md" />
-            <h2 className="text-[20px] font-bold text-primary mt-3">Belajar HTML</h2>
-            <p className="text-gray-500 dark:text-white">Sandhika Galih</p>
+    <div className="course-item-card flex flex-col gap-5 cursor-pointer">
+      <a href={link} className="block">
+        <Image
+          src={image}
+          alt={title}
+          width={500}
+          height={150}
+          className="rounded-md"
+        />
+        <div className="course-item-details flex flex-col gap-2 mt-3">
+          <h2 className="text-[20px] font-bold text-primary">{title}</h2>
+          <p className="text-gray-500 dark:text-white">{author}</p>
         </div>
-        <div className="course-item-card flex flex-col">
-            <Image src="/html/css-banner.jpg" width={500} height={150} className="rounded-md" />
-            <h2 className="text-[20px] font-bold text-primary mt-3">Belajar CSS</h2>
-            <p className="text-gray-500 dark:text-white">Sandhika Galih</p>
-        </div>
-        <div className="course-item-card flex flex-col">
-            <Image src="/html/javascript.jpg" width={500} height={150} className="rounded-md" />
-            <h2 className="text-[20px] font-bold text-primary mt-3">Belajar Javascript</h2>
-            <p className="text-gray-500 dark:text-white">Sandhika Galih</p>
-        </div>
-      </div>
-    </>
+      </a>
+    </div>
+  );
+};
+
+export default function CourseItemCards() {
+  return (
+    <div className="flex lg:flex-row md:flex-col gap-5">
+      {courseItemCardData.map((item) => (
+        <CourseItemCard key={item.title} {...item} />
+      ))}
+    </div>
   );
 }
-
-export default CourseItems;
