22563de198807e648c457575ddf9c9c7de37c31e
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-02-25 09:02:53 +0700

    update logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
     <div className="flex p-2 items-center justify-center">
-        <a href="/" className="text-primary lg:text-2xl md:text-xl uppercase font-bold">
+        <a href="/" className="text-primary text-2xl uppercase font-bold">
             WEBCADEMY
         </a>
     </div>
