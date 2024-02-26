cf12614e11f9fdb4e5fbf8e7efe8bad5628b3c63
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-02-26 21:02:01 +0700

    completing javascript and tailwind pages

--- app/(router)/tailwind/page.js
+++ app/(router)/tailwind/page.js
@@ -1,9 +1,106 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-function Tailwind() {
+const TailwindData = [
+  {
+    image: "/assets/tailwind/1.png",
+    title: "Apa itu Tailwind CSS ?",
+    part: "Part 1",
+    link: "/tailwind/1"
+  },
+  {
+    image: "/assets/tailwind/2.png",
+    title: "Kenapa Belajar Tailwind CSS ?",
+    part: "Part 2",
+    link: "./css"
+  },
+  {
+    image: "/assets/tailwind/3.png",
+    title: "Instalasi dan Konfigurasi Tailwind CSS",
+    part: "Part 3",
+    link: "./javascript"
+  },
+  {
+    image: "/assets/tailwind/4.png",
+    title: "Basic Utility Part 1",
+    part: "Part 4",
+    link: "./tailwind"
+  },
+  {
+    image: "/assets/tailwind/5.png",
+    title: "Basic Utility Part 2",
+    part: "Part 5",
+    link: "./tailwind"
+  },
+  {
+    image: "/assets/tailwind/6.png",
+    title: "Pseudo Class dan Dark Mode",
+    part: "Part 6",
+    link: "./tailwind"
+  },
+  {
+    image: "/assets/tailwind/7.png",
+    title: "Transform, Transition, dan Animation",
+    part: "Part 7",
+    link: "./tailwind"
+  },
+  {
+    image: "/assets/tailwind/8.png",
+    title: "Layouting",
+    part: "Part 8",
+    link: "./tailwind"
+  },
+  {
+    image: "/assets/tailwind/9.png",
+    title: "Responsive Design",
+    part: "Part 9",
+    link: "./tailwind"
+  },
+  {
+    image: "/assets/tailwind/10.png",
+    title: "Flexbox",
+    part: "Part 10",
+    link: "./tailwind"
+  },
+  {
+    image: "/assets/tailwind/11.png",
+    title: "Grid",
+    part: "Part 10",
+    link: "./tailwind"
+  },
+  {
+    image: "/assets/tailwind/12.png",
+    title: "Function dan Directives",
+    part: "Part 10",
+    link: "./tailwind"
+  },
+];
+
+const TailwindCard = ({ image, title, part, link }) => {
   return (
-    <div>Tailwind</div>
-  )
-}
+    <div className="flex flex-col gap-5 cursor-pointer">
+      <a href={link} className="block border border-primary rounded-md hover:shadow-md hover:shadow-primary transition duration-300 ease-in-out">
+        <Image
+          src={image}
+          alt={title}
+          width={500}
+          height={400}
+        />
+        <div className="course-item-details flex flex-col gap-2 p-3">
+          <h2 className="text-[20px] font-bold text-primary">{title}</h2>
+          <p className="text-gray-500 dark:text-white">{part}</p>
+        </div>
+      </a>
+    </div>
+  );
+};
 
-export default Tailwind
+export default function Tailwind() {
+  return (
+    <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3 gap-5 p-8">
+      {TailwindData.map((item) => (
+        <TailwindCard key={item.title} {...item} />
+      ))}
+    </div>
+  );
+}
