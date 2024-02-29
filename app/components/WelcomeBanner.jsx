072145def323ab1469c34c2a3ddf2881031437d5
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-02-29 15:05:39 +0700

    fixing clerk auth and welcome banner

--- app/components/WelcomeBanner.jsx
+++ app/components/WelcomeBanner.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
+import { currentUser } from '@clerk/nextjs'
 
-function WelcomeBanner() {
+async function WelcomeBanner() {
+
+  const user = await currentUser();
+
   return (
     <div className="flex gap-5 items-center bg-white dark:bg-slate-900 rounded-md p-5">
         <Image src="/banner.png" width={100} height={100} />
         <div>
-          <h2 className="text-[30px] font-bold">Selamat Datang di <span className="text-primary">Webcademy</span></h2>
-          <p className="text-gray-500 dark:text-white">Platform belajar pemrograman web bersama Pa Dhika 😁😃</p>
+          <h2 className="lg:text-[30px] font-bold md:text-2xl mb-4">Selamat Datang di Webcademy</h2>
+          <h2 className="lg:text-[30px] font-bold text-primary md:text-lg">{user.firstName} {user.lastName}</h2>
         </div>
     </div>
   )
