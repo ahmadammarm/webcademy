408771b2d9b3b74227e29310e4c3f2e0ff916bf8
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-03-04 21:12:08 +0700

    adding some javascript part

---
+++ app/(router)/javascript/3/Description.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Video from "./Video";
+
+function Description() {
+  return (
+    <div>
+      <h2 className="text-[20px] font-semibold">3. Bahasa Pemrograman</h2>
+      <h2 className="text-gray-400 text-[14px] mb-3">Sandhika Galih</h2>
+      {/* Video */}
+      <Video />
+
+      {/* Deskripsi */}
+      <h2 className="mt-5 text-[20px] font-semibold">Tentang Video ini</h2>
+      <p className="text-[14px] mt-5 italic">
+      Dalam video ini, kami membawa Anda ke dalam inti bahasa pemrograman JavaScript. Anda akan belajar tentang sintaks dasar JavaScript, seperti deklarasi variabel, tipe data, operator, dan struktur kontrol. Kami juga akan menjelaskan konsep-konsep penting seperti fungsi, objek, dan array, serta bagaimana Anda dapat menggunakannya dalam pembuatan aplikasi web yang dinamis. Bersiaplah untuk menjelajahi dunia JavaScript dan meningkatkan keterampilan pemrograman Anda melalui video ini!
+      </p>
+      <h2 className="mt-5 text-[20px] font-semibold">
+        Sumber Video :{" "}
+        <a
+          href="https://www.youtube.com/watch?v=Q2VqCG13ejA&list=PLFIM0718LjIX-K5eeHRImnZhPUMhsw9A7"
+          target="blank"
+          className="
+        text-blue-500
+        hover:underline
+        "
+        >
+          Klik Disini
+        </a>
+      </h2>
+    </div>
+  );
+}
+
+export default Description;
