43cbe889161f7ec0a08988f27a3e4b6b75012dd1
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-04-04 15:07:50 +0700

    update: update mode button

--- components/ui/toggle.jsx
+++ components/ui/toggle.jsx
@@ -5,33 +5,29 @@ import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          <Sun className="mr-4 h-4 w-4" /> Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-         <Moon className="mr-4 h-4 w-4" /> Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => {
+        setTheme(theme === "light" ? "dark" : "light")
+      }}
+    >
+      {theme === "light" ? (
+        <>
+          <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all border-gray-300" />
+          <span className="sr-only">Dark Mode</span>
+        </>
+      ) : (
+        <>
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all border-gray-900" />
+          <span className="sr-only">Light Mode</span>
+        </>
+      )}
+    </Button>
   )
 }
